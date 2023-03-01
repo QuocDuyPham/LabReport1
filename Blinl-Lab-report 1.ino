3f9fbda8a31bba1c3b4791f6dc8cacd84d00a509
QuocDuyPham <126590789+QuocDuyPham@users.noreply.github.com>
2023-02-28 16:29:18 -0800

    Update Blinl-Lab-report 1.ino

--- Blinl-Lab-report 1.ino
+++ Blinl-Lab-report 1.ino
@@ -1,8 +1,6 @@
 void setup() {
   pinMode(13, OUTPUT); // initialize digital pin 13 as an output.
 }
-
-// the loop function runs over and over again forever
 void loop() {
   digitalWrite(13, 1);  // turn the LED on (1 is the voltage level)
   delay(1000);                      // wait for a second
