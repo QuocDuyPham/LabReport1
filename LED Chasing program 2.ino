da5982bd415e6deee3db1b53ffb54afb79c93590
QuocDuyPham <126590789+QuocDuyPham@users.noreply.github.com>
2023-03-03 19:16:51 -0800

    Update LED Chasing program 2.ino

--- LED Chasing program 2.ino
+++ LED Chasing program 2.ino
@@ -1,27 +1,26 @@
 void setup() {
-  // initialize digital pins as outputa.
-  pinMode(13, OUTPUT);
-  pinMode(12, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(10, OUTPUT);
+  pinMode(13, OUTPUT);  // initialize pin 13 as output.
+  pinMode(12, OUTPUT);  // initialize pin 12 as output.
+  pinMode(11, OUTPUT);  // initialize pin 11 as output.
+  pinMode(10, OUTPUT);  // initialize pin 10 as output.
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-digitalWrite(13, 1);
-delay (100); 
-digitalWrite(13,0);
-delay (100); 
-digitalWrite(12, 1);
-delay (100); 
-digitalWrite(12,0);
-delay (100); 
-digitalWrite(11, 1);
-delay (100); 
-digitalWrite(11,0);
-delay (100); 
-digitalWrite(10, 1);
-delay (100); 
-digitalWrite(10,0);
-delay (100); 
+digitalWrite(13, 1); // Turn on the LED 13 
+delay (100);   // wait for a second
+digitalWrite(13,0); // Turn off the LED 13
+delay (100);  // wait for a second
+digitalWrite(12, 1); // Turn on the LED 12
+delay (100);  // wait for a second
+digitalWrite(12,0); // Turn off the LED 12
+delay (100);   // wait for a second
+digitalWrite(11, 1); // Turn on the LED 11
+delay (100);  // wait for a second
+digitalWrite(11,0); // Turn off the LED 11
+delay (100);  // wait for a second
+digitalWrite(10, 1); // Turn on the LED 10
+delay (100);  // wait for a second
+digitalWrite(10,0); // Turn off the LED 10
+delay (100);  // wait for a second
 }
