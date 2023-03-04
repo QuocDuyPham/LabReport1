3238d8c064954ce7201766ab5074849adc1399a4
QuocDuyPham <126590789+QuocDuyPham@users.noreply.github.com>
2023-03-03 19:47:05 -0800

    Update PB not pressed RG on PB pressed RB on.ino

--- PB not pressed RG on PB pressed RB on.ino
+++ PB not pressed RG on PB pressed RB on.ino
@@ -14,14 +14,14 @@ void loop() {
   int BinValue = digitalRead(PushButton);   //read the pushbutton value into a variable
   Serial.print(" BinValue = " ); // print out the line "BinValue = "
   Serial.println(BinValue);  //print out the value of the pushbutton
-  if (BinValue == 0)
-  { digitalWrite(PIN_RED,   1);
-  digitalWrite(PIN_GREEN, 1);
-  digitalWrite(PIN_BLUE,  0);
+  if (BinValue == 0) // condition for " if " fuction 
+  { digitalWrite(PIN_RED,   1); // when  the condition is statisfied the PIN_RED turn on
+  digitalWrite(PIN_GREEN, 1); // when  the condition is statisfied the PIN_GREEN turn on
+  digitalWrite(PIN_BLUE,  0); // when  the condition is statisfied the PIN_BLUE turn on
   }
-  else{
-  digitalWrite(PIN_RED,   1);
-  digitalWrite(PIN_GREEN, 0);
-  digitalWrite(PIN_BLUE,  1);  
+  else{ // when the condition for the if fuction is not statisfied 
+  digitalWrite(PIN_RED,   1); // turn on PIN_RED
+  digitalWrite(PIN_GREEN, 0); // Turn off PIN_GREEN
+  digitalWrite(PIN_BLUE,  1); // Turn on PIN_BLUE
   }
 }
