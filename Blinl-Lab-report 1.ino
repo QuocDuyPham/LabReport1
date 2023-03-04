8acdf64d491b6b1fa0aebf97035cc9ade21188e1
QuocDuyPham <126590789+QuocDuyPham@users.noreply.github.com>
2023-03-03 18:38:08 -0800

    Update Blinl-Lab-report 1.ino

--- Blinl-Lab-report 1.ino
+++ Blinl-Lab-report 1.ino
@@ -2,8 +2,8 @@ void setup() {
   pinMode(13, OUTPUT); // initialize digital pin 13 as an output.
 }
 void loop() {
-  digitalWrite(13, 1);  // turn the LED on (1 is the voltage level)
+  digitalWrite(13, 1);  // turn the LED on 
   delay(1000);                      // wait for a second
-  digitalWrite(13, 0);   // turn the LED off (0 is voltage level low)
+  digitalWrite(13, 0);   // turn the LED off
   delay(1000);                      // wait for a second
 }
