facd41f3c6e76f56d8af6074dd52d156fd869d6f
QuocDuyPham <126590789+QuocDuyPham@users.noreply.github.com>
2023-03-10 10:33:16 -0800

    Update PB not pressed RG on PB pressed RB on.ino

--- PB not pressed RG on PB pressed RB on.ino
+++ PB not pressed RG on PB pressed RB on.ino
@@ -4,7 +4,7 @@ const int PIN_GREEN = 6;
 const int PIN_BLUE  = 7;
 void setup() {
   Serial.begin(9600);   //start serial connection
-  pinMode(PushButton, INPUT_PULLUP);  //configure pin 2 as an input and enable the internal pull-up resistor
+  pinMode(PushButton, INPUT_PULLUP);  //configure pin 13 as an input and enable the internal pull-up resistor
   pinMode(PIN_RED,   OUTPUT); //configure PIN_RED as an Output 
   pinMode(PIN_GREEN, OUTPUT); //configure PIN_GREEN as an input 
   pinMode(PIN_BLUE,  OUTPUT); //configure PIN_BLUE as an input 
@@ -15,13 +15,14 @@ void loop() {
   Serial.print(" BinValue = " ); // print out the line "BinValue = "
   Serial.println(BinValue);  //print out the value of the pushbutton
   if (BinValue == 0) // condition for " if " fuction 
-  { digitalWrite(PIN_RED,   1); // when  the condition is statisfied the PIN_RED turn on
-  digitalWrite(PIN_GREEN, 1); // when  the condition is statisfied the PIN_GREEN turn on
-  digitalWrite(PIN_BLUE,  0); // when  the condition is statisfied the PIN_BLUE turn on
+  // RGB turn yellow
+  { analogWrite(PIN_RED,   255); // when  the condition is statisfied the PIN_RED turn on
+  analogWrite(PIN_GREEN, 255); // when  the condition is statisfied the PIN_GREEN turn on
+  analogWrite(PIN_BLUE,  0); // when  the condition is statisfied the PIN_BLUE turn OFF
   }
-  else{ // when the condition for the if fuction is not statisfied 
-  digitalWrite(PIN_RED,   1); // turn on PIN_RED
-  digitalWrite(PIN_GREEN, 0); // Turn off PIN_GREEN
-  digitalWrite(PIN_BLUE,  1); // Turn on PIN_BLUE
+  else{ // when the condition for the if fuction is not statisfied (RGB turn PINK)
+  analogWrite(PIN_RED,   255); // turn on PIN_RED
+  analogWrite(PIN_GREEN, 0); // Turn off PIN_GREEN
+  analogWrite(PIN_BLUE,  255); // Turn on PIN_BLUE
   }
 }
